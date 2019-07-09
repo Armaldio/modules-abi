c9e9dc5e0bc1eee7b49bb2500e1f1d026d09c0e0
Quentin Goinaud <armaldio@gmail.com>
2019-07-09 23:46:12 +0200

    improvements
    add tests about range
    deploy rangefunction to now

--- api/index.js
+++ api/index.js
@@ -1,11 +1,29 @@
-module.exports = (req, res) => {
+const abis = require('../src');
+
+module.exports = async (req, res) => {
 	console.log(req.query);
 
 	const {mode, runtime, target, abi} = req.query;
 
 	switch (mode) {
+		case 'range':
+			if (runtime && abi) {
+				const versions = await abis.getRange(runtime, parseInt(abi, 10));
+				console.log(versions);
+				res.json(versions);
+			} else {
+				res.end('required: runtime, abi');
+			}
 
+			break;
+		case 'abi':
+			break;
+		case 'target':
+			break;
+		default:
+			res.end('Unsupported mode, try one of: range');
+			break;
 	}
 
-	res.send('OK');
+	res.end('OK');
 };
--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ test('import does not throw', t => {
 });
 
 for (const runtime of abis.runtimes) {
+	// Get abi
 	for (const [versionToCheck, result] of fixtures[runtime.name].abisCheck) {
 		test(`check ${runtime.name} version v${versionToCheck}`, async t => {
 			const abi = await abis.getABI(runtime.name, versionToCheck);
@@ -16,9 +17,18 @@ for (const runtime of abis.runtimes) {
 		});
 	}
 
+	// Get target
 	for (const [abiToCheck, result] of fixtures[runtime.name].versionsCheck) {
 		test(`check ${runtime.name} abi ${abiToCheck}`, async t => {
 			const abi = await abis.getTarget(runtime.name, abiToCheck);
+			t.deepEqual(abi, result, 'Target are not the same');
+		});
+	}
+
+	// Get range
+	for (const [abiToCheck, result] of fixtures[runtime.name].rangeCheck) {
+		test(`check ${runtime.name} range of abi ${abiToCheck}`, async t => {
+			const abi = await abis.getRange(runtime.name, abiToCheck);
 			t.deepEqual(abi, result, 'Target are not the same');
 		});
 	}
